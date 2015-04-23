061414b949a3e6ba05aaf4f266feadcb2362881a
Michael Pollind <m_pollen@yahoo.com>
2015-04-23 10:38:51 -0700

    first

---
+++ www/js/foodController.js
@@ -0,0 +1,16 @@
+app.controller("foodController", function($http, $scope, $stateParams) {
+
+
+    $http.get(URL + 'rest/food?id=' + $stateParams.foodId, {}).
+    success(function(data, status, headers, config) {
+        console.log(data);
+        $scope.name = data.data.name;
+        $scope.image = data.data.image;
+        $scope.nutrional = data.data.nutritionImage;
+    }).
+    error(function(data, status, headers, config) {
+
+    });
+
+
+});
